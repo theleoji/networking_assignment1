2bdf14e7133032edde6697bff01ddc60392b62fd
Leo Ji <leo-ji@users.noreply.github.com>
2019-01-25 20:47:32 -0600

    many things

--- http_client.py
+++ http_client.py
@@ -2,6 +2,8 @@ import sys
 import socket
 from urlparse import urlparse
 
+
+
 # Checking if the input is of the right form.
 if len(sys.argv) != 2:
     sys.exit(-1)
@@ -16,16 +18,24 @@ if(enteredUrl[0:7] != 'http://'):
 #Printing the URL to screen for the user to see.
 # print(enteredUrl)
 
-def accessRequest(enteredUrl):
+def accessRequest(enteredUrl, counter):
+
+    # dies on 10
+    if (counter > 9):
+        print("10 redirects, exiting")
+        return ("", 10)
+
 #Parsing through the URL input using the URLparse library.
+    exitCode = 0
     o = urlparse(enteredUrl)
+
     httpMsg = "GET "
 
     if(o.scheme == 'https'):
         print ("Attempted to HTTPS")
         sys.exit(403)
 
-    host = (o.netloc, 80)
+    host = (o.netloc, port)
 
     if (o.path==""):
         httpMsg += "/"
@@ -59,8 +69,13 @@ def accessRequest(enteredUrl):
     # looking at HTTP response code
     firstLine = msgReturn.splitlines()[0]
     newUrl = ""
-    # print(firstLine)
-    if firstLine.find("301 Moved Permanently"):
+    # print(int(firstLine[9:12]))
+
+    responseCode = int(firstLine[9:12])
+
+    # print(type(responseCode))
+
+    if (responseCode == 301 or responseCode == 302):
         # print(firstLine)
         for line in msgReturn.splitlines():
             loc = line.find("Location: ")
@@ -68,20 +83,38 @@ def accessRequest(enteredUrl):
                 # print(loc)
                 newUrl = line[10:]
                 break
+    # elif firstLine.find("302 Moved Temporarily"):
+    #     for line in msgReturn.splitlines():
+    #         loc = line.find("Location: ")
+    #         if (loc != -1):
+    #             newUrl = line[10:]
+    #             break
 
-    elif firstLine.find("302 Moved Temporarily"):
-        for line in msgReturn.splitlines():
-            loc = line.find("Location: ")
-            if (loc != -1):
-                newUrl = line[10:]
-                break
+    if (responseCode >= 400 and responseCode < 500):
+        # print(responseCode)
+        exitCode = responseCode
+
+    contentType = msgReturn.find("Content-Type: text/html")
+    if(contentType == -1):
+        print("Returned type is not text/html")
+        sys.exit(101)
 
     socketObj.close()
 
     # print(newUrl)
     if(newUrl):
-        msgReturn = accessRequest(newUrl)
+        print("Redirected to: " + newUrl)
+        (msgReturn, exitCode) = accessRequest(newUrl, counter + 1)
 
-    return msgReturn
+    return (msgReturn, exitCode)
 
-print(accessRequest(enteredUrl))
+(msgReturn, exitCode) = accessRequest(enteredUrl , 0)
+
+# from https://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python
+print(str(msgReturn).decode('string_escape'))
+
+if (int(exitCode) > 0):
+    # print(exitCode)
+    sys.exit(exitCode)
+
+sys.exit(0)
