0fcdba2833b5017a5f9a8246a218ec14dc92de1a
LJGladic <lukasgladic2017@u.northwestern.edu>
2019-01-25 19:21:10 -0600

    added comments

--- http_client.py
+++ http_client.py
@@ -2,13 +2,17 @@ import sys
 import socket
 from urlparse import urlparse
 
+# Checking if the input is of the right form.
 if len(sys.argv) != 2:
     exit(-1)
 
+#Storing the inputed URL.
 enteredUrl = sys.argv[1]
 
+#Printing the URL to screen for the user to see.
 print(enteredUrl)
 
+#Parsing through the URL input using the URLparse library.
 o = urlparse(enteredUrl)
 
 host = ("eecs.northwestern.edu", 80)
