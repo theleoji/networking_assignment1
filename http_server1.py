259effcb656167152e8e4e3ad8d1f8c3dece4417
Leo Ji <leo-ji@users.noreply.github.com>
2019-01-27 17:14:38 -0600

    pushhhhh

--- http_server1.py
+++ http_server1.py
@@ -3,13 +3,43 @@
 
 import sys
 import socket
+import os
+
+# https://stackoverflow.com/questions/30686701/python-get-size-of-string-in-bytes
+def utf8len(s):
+    return len(s.encode('utf-8'))
+
+def httpRead(requestMsg):
+    print(requestMsg)
+    httpParts = requestMsg.split()
+
+    if (httpParts[0] != "GET"):
+        sys.exit(101)
+
+    path = httpParts[1]
+    filename = path[1:]
+
+    return fileSearch(filename)
+
+def fileSearch(filename):
+
+    # https://stackoverflow.com/questions/11968976/list-files-only-in-the-current-directory
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        if (f == filename):
+            if (f[-5:]==".html" or f[-4]==".htm"):
+                fileContent = open(f, "r")
+                return (fileContent, 200)
+            else:
+                return ("", 403)
+
+    return ("", 404)
 
 # Checking if the input is of the right form
 if len(sys.argv) != 2:
     sys.exit(-1)
 
-#Storing the inputed URL.
-HOST = '127.0.0.1'
+HOST = ""
 portRequested = int(sys.argv[1])
 
 # basic server adapted from https://realpython.com/python-sockets/#echo-server
@@ -22,11 +52,42 @@ s.listen(5)
 conn, addr = s.accept()
 
 print('Connected by ', addr)
+
+data = ""
+
 while True:
-    data = conn.recv(1024)
+    data += conn.recv(1024)
+    print(data)
     if not data:
         break
-    conn.sendall(data)
+
+print(data)
+
+fileContent, responseCode = httpRead(data)
+
+responseAll == "HTTP/1.0 "
+
+if(responseCode == 403):
+    responseAll += "403 Forbidden"
+    fileContent = "<h1>403 Forbidden</h1>"
+elif(responseCode == 404):
+    responseAll += "404 Not Found"
+    fileContent = "<h1>404 Not Found</h1>"
+else:
+    responseAll += "200 OK"
+
+responseAll += "\r\n"
+responseAll += "Content-Type: text/html"
+responseAll += "\r\n"
+responseAll += "Content-Length: "
+responseAll += utf8len(fileContent)
+responseAll += "\r\n"
+
+responseAll += "\r\n"
+responseAll += fileContent
+# full response constructed
+
+conn.sendall(fileContent)
 
 conn.close()
 s.close()
