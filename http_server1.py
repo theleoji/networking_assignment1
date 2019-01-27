e9397c4f72333a1c33f4187b28036866249d6aec
Leo Ji <leo-ji@users.noreply.github.com>
2019-01-27 16:11:48 -0600

    basic server

--- http_server1.py
+++ http_server1.py
@@ -1,5 +1,29 @@
 # http_server1.py written by Leo Ji (ljd753) and Lukas J. Gladic (ljg766)
 # for EECS 340 Winter 2019 with Professor Yan Chen
 
+import sys
+import socket
 
-print("fuck")
+# Checking if the input is of the right form
+if len(sys.argv) != 2:
+    sys.exit(-1)
+
+#Storing the inputed URL.
+portRequested = sys.argv[1]
+
+# basic server adapted from https://realpython.com/python-sockets/#echo-server
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, portRequested))
+s.listen()
+
+conn, addr = s.accept()
+
+with conn:
+    print('Connected by ', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
